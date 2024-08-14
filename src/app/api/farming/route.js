c49cdcd001457e22f309b84359bf4390032c36ae
Joaquin Meneses <joakinmeneses.jm@gmail.com>
2024-08-14 00:18:29 -0300

    fix env

--- src/app/api/farming/route.js
+++ src/app/api/farming/route.js
@@ -8,7 +8,7 @@ const fetchData = async (url, query) => {
     cache: "no-store",
     headers: {
       "Content-Type": "application/json",
-      "x-api-key": process.env.apiKeySkyMavis,
+      "x-api-key": process.env.NEXT_PUBLIC_API_KEY_SKY_MAVIS,
     },
     body: JSON.stringify({ query }),
   });
