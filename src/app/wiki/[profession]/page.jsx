070fe36b1d3d169cbf3b320e905285dbfc1e3ffc
Joaquin Meneses <joakinmeneses.jm@gmail.com>
2024-08-23 21:43:43 -0300

    create template items

--- src/app/wiki/[profession]/page.jsx
+++ src/app/wiki/[profession]/page.jsx
@@ -39,160 +39,140 @@ const Profession = ({ params }) => {
 
   return (
     <main className="flex flex-grow">
-      <div className="flex h-full w-full flex-wrap justify-center gap-2 p-2">
-        {data?.map((item, index) => {
-          return (
-            <Box
-              key={index}
-              sx={{
-                display: "flex",
-                gap: 1,
-                alignItems: "center",
-              }}
-            >
-              <Box
-                sx={{
-                  width: "25%",
-                }}
+      <Box
+        sx={{
+          width: "60%",
+          padding: 2,
+          display: "flex",
+          justifyContent: "center",
+          flexWrap: "wrap",
+          gap: 1,
+        }}
+      >
+        {data
+          ?.sort((a, b) => a.requiresLevel - b.requiresLevel)
+          .map((item, index) => {
+            return (
+              <Card
+                key={index}
+                variant="plain"
+                sx={{ cursor: "pointer", width: 1 / 9 }}
+                onClick={() => setSelectedItem(item)}
               >
-                <Box
-                  sx={{
-                    display: "grid",
-                    gridTemplateColumns: "repeat(1, 1fr)",
-                    gap: "10px",
-                  }}
-                >
-                  <Card
-                    key={item.name}
-                    variant="plain"
-                    sx={{ cursor: "pointer" }}
-                    onClick={() => setSelectedItem(item)}
-                  >
-                    <CardContent>
-                      <AspectRatio ratio="1" sx={{ width: 60 }}>
-                        <img
-                          src={item.cdnImage}
-                          loading="lazy"
-                          alt={item.name}
-                        />
-                      </AspectRatio>
-                    </CardContent>
-                  </Card>
-                </Box>
-              </Box>
-            </Box>
-          );
-        })}
-      </div>
-      <div className="flex h-full w-full flex-wrap justify-center gap-2 p-2">
-        <Box sx={{ width: "70%", padding: "15px" }}>
-          {selectedItem ? (
-            <Card variant="plain">
-              <CardContent>
-                <Typography level="h4">{selectedItem.name}</Typography>
-                <AspectRatio objectFit="contain" sx={{ width: "100%", mb: 2 }}>
-                  {selectedItem.cdnImage ? (
-                    <img
-                      src={selectedItem.cdnImage}
-                      loading="lazy"
-                      alt={selectedItem.name}
-                    />
-                  ) : (
-                    <Typography>No Image Available</Typography>
-                  )}
-                </AspectRatio>
-                <Typography level="body1">
-                  Require Level: {selectedItem.requiresLevel}
+                <CardContent>
+                  <AspectRatio ratio="1" sx={{ width: "100%" }}>
+                    <img src={item.cdnImage} loading="lazy" alt={item.name} />
+                  </AspectRatio>
+                </CardContent>
+              </Card>
+            );
+          })}
+      </Box>
+      <Box sx={{ flexGrow: "inherit", padding: 2 }}>
+        {selectedItem ? (
+          <Card variant="plain">
+            <CardContent>
+              <Typography level="h4">{selectedItem.name}</Typography>
+              <AspectRatio objectFit="contain" sx={{ width: "100%" }}>
+                {selectedItem.cdnImage ? (
+                  <img
+                    src={selectedItem.cdnImage}
+                    loading="lazy"
+                    alt={selectedItem.name}
+                  />
+                ) : (
+                  <Typography>No Image Available</Typography>
+                )}
+              </AspectRatio>
+              <Typography level="body1">
+                Require Level: {selectedItem.requiresLevel}
+              </Typography>
+              <Typography level="body1" sx={{ display: "flex", gap: 0.5 }}>
+                Floor Price:
+                <Typography level="body2">
+                  {selectedItem.prices.ron} RON
                 </Typography>
-                <Typography level="body1" sx={{ display: "flex", gap: 0.5 }}>
-                  Floor Price:
+                <Typography level="body2">
+                  {selectedItem.prices.usd} USD
+                </Typography>
+              </Typography>
+              <Typography level="body1" sx={{ mt: 2 }}>
+                Stats:
+              </Typography>
+              {selectedItem.stats?.map((stat, index) => (
+                <Box key={index}>
                   <Typography level="body2">
-                    {selectedItem.prices.ron} RON
+                    {stat.name}: {stat.values.min} - {stat.values.max}
                   </Typography>
-                  <Typography level="body2">
-                    {selectedItem.prices.usd} USD
-                  </Typography>
-                </Typography>
-                <Typography level="body1" sx={{ mt: 2 }}>
-                  Stats:
-                </Typography>
-                {selectedItem.stats?.map((stat, index) => (
-                  <Box key={index}>
-                    <Typography level="body2">
-                      {stat.name}: {stat.values.min} - {stat.values.max}
-                    </Typography>
-                  </Box>
-                ))}
-                <Typography level="body1" sx={{ mt: 2 }}>
-                  Recipe:
-                </Typography>
-                <Box
-                  sx={{ display: "flex", alignItems: "center", gap: 1, mb: 2 }}
-                >
-                  {selectedItem.recipe.materials.map((material, index) => (
-                    <React.Fragment key={index}>
-                      <Box
-                        sx={{ display: "flex", alignItems: "center", gap: 1 }}
-                      >
-                        <Image
-                          src={material.cdnImage}
-                          alt={material.name}
-                          width={40}
-                          height={40}
-                        />
-                        <Typography level="body2">
-                          x {material.quantity}
-                        </Typography>
-                      </Box>
-                      {index < selectedItem.recipe.materials.length - 1 && (
-                        <Typography level="body2" sx={{ mx: 1 }}>
-                          +
-                        </Typography>
-                      )}
-                    </React.Fragment>
-                  ))}
                 </Box>
-                <Box
-                  sx={{ display: "flex", alignItems: "center", gap: 2, mb: 2 }}
-                >
-                  {/* Resumen de Precios y Energía */}
-                  <Typography level="body2">
-                    Total Recipe Price:
-                    <Box sx={{ display: "flex", gap: 0.5 }}>
+              ))}
+              <Typography level="body1" sx={{ mt: 2 }}>
+                Recipe:
+              </Typography>
+              <Box
+                sx={{ display: "flex", alignItems: "center", gap: 1, mb: 2 }}
+              >
+                {selectedItem.recipe.materials.map((material, index) => (
+                  <React.Fragment key={index}>
+                    <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
                       <Image
-                        src="https://cdn.skymavis.com/ronin/2020/ron/logo.png"
-                        alt="Ron"
-                        width={20}
-                        height={20}
+                        src={material.cdnImage}
+                        alt={material.name}
+                        width={40}
+                        height={40}
                       />
-                      {selectedItem.recipe.minPriceTotalRon}
-                      <Image
-                        src="https://cdn.skymavis.com/ronin/2020/erc20/0x0b7007c13325c48911f73a2dad5fa5dcbf808adc/logo.png"
-                        alt="Ron"
-                        width={20}
-                        height={20}
-                      />
-                      {selectedItem.recipe.minPriceTotalUsd}
+                      <Typography level="body2">
+                        x {material.quantity}
+                      </Typography>
                     </Box>
-                  </Typography>
-                  <Typography level="body2">
-                    Total Energy Required:{" "}
-                    {selectedItem.recipe.totalRequireEnergy}
-                  </Typography>
-                  <Typography level="body2">Total Energy Cost:</Typography>
-                  <Typography level="body2">
-                    USD: {selectedItem.recipe.totalEnergyCost}
-                  </Typography>
-                </Box>
-              </CardContent>
-            </Card>
-          ) : (
-            <Typography level="body1">
-              Selecciona un item para ver su información.
-            </Typography>
-          )}
-        </Box>
-      </div>
+                    {index < selectedItem.recipe.materials.length - 1 && (
+                      <Typography level="body2" sx={{ mx: 1 }}>
+                        +
+                      </Typography>
+                    )}
+                  </React.Fragment>
+                ))}
+              </Box>
+              <Box
+                sx={{ display: "flex", alignItems: "center", gap: 2, mb: 2 }}
+              >
+                {/* Resumen de Precios y Energía */}
+                <Typography level="body2">
+                  Total Recipe Price:
+                  <Box sx={{ display: "flex", gap: 0.5 }}>
+                    <Image
+                      src="https://cdn.skymavis.com/ronin/2020/ron/logo.png"
+                      alt="Ron"
+                      width={20}
+                      height={20}
+                    />
+                    {selectedItem.recipe.minPriceTotalRon}
+                    <Image
+                      src="https://cdn.skymavis.com/ronin/2020/erc20/0x0b7007c13325c48911f73a2dad5fa5dcbf808adc/logo.png"
+                      alt="Ron"
+                      width={20}
+                      height={20}
+                    />
+                    {selectedItem.recipe.minPriceTotalUsd}
+                  </Box>
+                </Typography>
+                <Typography level="body2">
+                  Total Energy Required:{" "}
+                  {selectedItem.recipe.totalRequireEnergy}
+                </Typography>
+                <Typography level="body2">Total Energy Cost:</Typography>
+                <Typography level="body2">
+                  USD: {selectedItem.recipe.totalEnergyCost}
+                </Typography>
+              </Box>
+            </CardContent>
+          </Card>
+        ) : (
+          <Typography level="body1">
+            Selecciona un item para ver su información.
+          </Typography>
+        )}
+      </Box>
     </main>
   );
 };
